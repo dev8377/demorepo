a75b1dbb3b5ff047b13aeb1853259794626ce043
dev8377 <devgithub018@gmail.com>
2026-05-03 11:57:22 +0530

    Add files via upload

--- array.py
+++ array.py
@@ -9,4 +9,25 @@ for i in arr:
     print(i,end=" ")
 
 #insertion
+value=int(input("\nEnter the element to be inserted: "))
+pos=int(input("Enter the positions to insert the element: "))
+arr.insert(pos,value)
+print("Array after insertion ")
+for i in arr:
+    print(i,end=" ")
 
+#Deletion by value
+d=int(input("\nEnter the value to be deleted: "))
+arr.remove(d)
+print("Array after deletion")
+for i in arr:
+    print(i,end=" ")
+
+#deletion by index or position
+d=int(input("\nEnter the position(index) of element to be deleted: "))
+for i in range(len(arr)):
+    if i==d:
+        arr.pop(d)
+print("Array after deletion ")
+for i in arr:
+    print(i,end=" ")
