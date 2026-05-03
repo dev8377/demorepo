66537538393ef812de4e9385bd1e9835b870e00d
dev8377 <devgithub018@gmail.com>
2026-05-03 11:13:05 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,12 @@
+#traversal
+n=int(input("Enter the no of elements: "))
+arr=[]
+for i in range(n):
+    a=int(input("Enter the elements: "))
+    arr.append(a)
+print("Array elements: ")
+for i in arr:
+    print(i,end=" ")
+
+#insertion
+
